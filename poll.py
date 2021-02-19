c6a2e57a5c6a96694a8f97bd48a975d368240f15
Finlay Wilders <fwilders@cisco.com>
2021-02-19 17:17:42 +0000

    Updated test for evaluating if health has changed. Only takes overall health, total Wired & wireless healths into account.

--- poll.py
+++ poll.py
@@ -12,19 +12,32 @@ token = dnapp.get_token()
 # header field for requests
 headers={'X-Auth-Token': '{}'.format(token)}
 
-# Get initial client health info
-previous = dnapp.get_client_health(headers)
-
+# Reminder how to stop process.
 print('\n\n\x1b[0;30;41m' + 'Press ctrl+C (^C) to stop.' + '\x1b[0m\n\n')
 
+# Get initial client health info
+previous = dnapp.get_client_health(headers)
+print(dnapp.format_client_health(previous))
+
 # poll dnac every 5 seconds, print whether client health is
-# 'same' as last time (5 seconds ago) or 'not same'.
-while True:
-    sleep(30)
-    current = dnapp.get_client_health(headers)
-    if current != previous:
-        print('Not Same.')
-        print(dnapp.format_client_health(current))
-        current = previous
-    else:
-        print('Same.')
+# 'same' as last time (30 seconds ago) or 'not same'.
+def main ():
+    while True:
+        sleep(30)
+        current = dnapp.get_client_health(headers)
+        if tests(current, previous) == False:
+            print('Not Same.')
+            print(dnapp.format_client_health(current))
+            current = previous
+        else:
+            print('Same.')
+
+def tests(cur, pre):
+    cur1 = cur['response'][0]['scoreDetail']
+    pre1 = pre['response'][0]['scoreDetail']
+    for i in cur1:
+        if i['scoreValue'] != i['scoreValue']:
+            return False
+
+if __name__ == '__main__':
+    main()
